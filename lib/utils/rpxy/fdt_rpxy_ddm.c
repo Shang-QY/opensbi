65399b1cc24ddfd79ead6b31981fe196e6f79d36
Qingyu Shang <2931013282@sjtu.edu.cn>
2023-11-01 02:04:18 +0800

    rpxy: refine format
    Signed-off-by: Qingyu Shang <2931013282@sjtu.edu.cn>

--- lib/utils/rpxy/fdt_rpxy_ddm.c
+++ lib/utils/rpxy/fdt_rpxy_ddm.c
@@ -36,10 +36,10 @@ struct ddm_rpxy_dispatcher {
                                                 MM_VERSION_MINOR)
 
 struct efi_param_header {
-	uint8_t type;	/* type of the structure */
+	uint8_t type;	 /* type of the structure */
 	uint8_t version; /* version of this structure */
-	uint16_t size;   /* size of this structure in bytes */
-	uint32_t attr;   /* attributes: unused bits SBZ */
+	uint16_t size;	 /* size of this structure in bytes */
+	uint32_t attr;	 /* attributes: unused bits SBZ */
 };
 
 struct efi_secure_partition_cpu_info {
@@ -75,31 +75,31 @@ struct efi_secure_shared_buffer {
 static void mm_setup_boot_info(uint64_t a1)
 {
 	struct efi_secure_shared_buffer *mm_shared_buffer = (void *)a1;
-    struct efi_secure_partition_boot_info *mm_boot_info =
-                            &mm_shared_buffer->mm_payload_boot_info;
+	struct efi_secure_partition_boot_info *mm_boot_info =
+		&mm_shared_buffer->mm_payload_boot_info;
 	mm_boot_info->header.version = 0x01;
-	mm_boot_info->dd_mem_base	= 0x80C00000;
-	mm_boot_info->dd_mem_limit	= 0x82000000;
-	mm_boot_info->dd_image_base = 0x80C00000;
-    /* Stack from (dd_heap_base + dd_heap_size) to dd_shared_buf_base */
-	mm_boot_info->dd_stack_base = 0x81F7FFFF;
-	mm_boot_info->dd_heap_base = 0x80F00000;
-	mm_boot_info->dd_ns_comm_buf_base = 0xFFE00000;
-	mm_boot_info->dd_shared_buf_base = 0x81F80000;
-	mm_boot_info->dd_image_size	 = 0x300000;
-	mm_boot_info->dd_pcpu_stack_size = 0x10000;
-	mm_boot_info->dd_heap_size	 = 0x800000;
-	mm_boot_info->dd_ns_comm_buf_size = 0x200000;
-	mm_boot_info->dd_shared_buf_size = 0x80000;
-	mm_boot_info->num_dd_mem_region = 0x6;
-	mm_boot_info->num_cpus	 = 1;
-	mm_shared_buffer->mm_cpu_info[0].linear_id		 = 0;
-	mm_shared_buffer->mm_cpu_info[0].flags		 = 0;
+	mm_boot_info->dd_mem_base    = 0x80C00000;
+	mm_boot_info->dd_mem_limit   = 0x82000000;
+	mm_boot_info->dd_image_base  = 0x80C00000;
+	/* Stack from (dd_heap_base + dd_heap_size) to dd_shared_buf_base */
+	mm_boot_info->dd_stack_base		   = 0x81F7FFFF;
+	mm_boot_info->dd_heap_base		   = 0x80F00000;
+	mm_boot_info->dd_ns_comm_buf_base	   = 0xFFE00000;
+	mm_boot_info->dd_shared_buf_base	   = 0x81F80000;
+	mm_boot_info->dd_image_size		   = 0x300000;
+	mm_boot_info->dd_pcpu_stack_size	   = 0x10000;
+	mm_boot_info->dd_heap_size		   = 0x800000;
+	mm_boot_info->dd_ns_comm_buf_size	   = 0x200000;
+	mm_boot_info->dd_shared_buf_size	   = 0x80000;
+	mm_boot_info->num_dd_mem_region		   = 0x6;
+	mm_boot_info->num_cpus			   = 1;
+	mm_shared_buffer->mm_cpu_info[0].linear_id = 0;
+	mm_shared_buffer->mm_cpu_info[0].flags	   = 0;
 	mm_boot_info->cpu_info = mm_shared_buffer->mm_cpu_info;
 }
 
 int find_domain(void *fdt, int nodeoff, const char *compatible,
-            struct sbi_domain **output_domain)
+		struct sbi_domain **output_domain)
 {
 	u32 i;
 	const u32 *val;
@@ -118,11 +118,11 @@ int find_domain(void *fdt, int nodeoff, const char *compatible,
 	}
 
 	/* Read DT node name and find match */
-	strncpy(name, fdt_get_name(fdt, domain_offset, NULL),
-			sizeof(name));
+	strncpy(name, fdt_get_name(fdt, domain_offset, NULL), sizeof(name));
 	name[sizeof(name) - 1] = '\0';
 
-	sbi_domain_for_each(i, dom) {
+	sbi_domain_for_each(i, dom)
+	{
 		if (!sbi_strcmp(dom->name, name)) {
 			*output_domain = dom;
 			return SBI_SUCCESS;
@@ -134,10 +134,10 @@ int find_domain(void *fdt, int nodeoff, const char *compatible,
 
 struct ddm_rpxy_dispatcher mm_dispatcher;
 
-int ddm_mm_setup(void *fdt, int nodeoff,
-			  const struct fdt_match *match)
+int ddm_mm_setup(void *fdt, int nodeoff, const struct fdt_match *match)
 {
-	if (find_domain(fdt, nodeoff, "opensbi-domain-instance", &mm_dispatcher.dom))
+	if (find_domain(fdt, nodeoff, "opensbi-domain-instance",
+			&mm_dispatcher.dom))
 		return SBI_EINVAL;
 
 	mm_setup_boot_info(mm_dispatcher.dom->next_arg1);
@@ -145,16 +145,16 @@ int ddm_mm_setup(void *fdt, int nodeoff,
 	return 0;
 }
 
-static int ddm_mm_dispatch(struct sbi_rpxy_service_group *grp, 
-                  struct sbi_rpxy_service *srv,
+static int ddm_mm_dispatch(struct sbi_rpxy_service_group *grp,
+				  struct sbi_rpxy_service *srv,
 				  void *tx, u32 tx_len,
 				  void *rx, u32 rx_len,
 				  unsigned long *ack_len)
 {
-    int srv_id = srv->id;
+	int srv_id = srv->id;
 
 	if (RPMI_MM_SRV_MM_VERSION == srv_id) {
-		*((int32_t *)rx) = 0;
+		*((int32_t *)rx)		       = 0;
 		*((uint32_t *)(rx + sizeof(uint32_t))) = MM_VERSION_COMPILED;
 	} else if (RPMI_MM_SRV_MM_COMMUNICATE == srv_id) {
 		sbi_dynamic_domain_entry(mm_dispatcher.dom->index);
