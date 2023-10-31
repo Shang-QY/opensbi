823f9913871393d406004612487741ae3359c9ff
Qingyu Shang <2931013282@sjtu.edu.cn>
2023-11-01 02:40:56 +0800

    lib/sbi_domain: add dynamic domain management(DDM)
    Signed-off-by: Qingyu Shang <2931013282@sjtu.edu.cn>

--- lib/sbi/sbi_init.c
+++ lib/sbi/sbi_init.c
@@ -395,6 +395,8 @@ static void __noreturn init_coldboot(struct sbi_scratch *scratch, u32 hartid)
 
 	sbi_boot_print_hart(scratch, hartid);
 
+	sbi_dynamic_domain_init(scratch, true);
+
 	/*
 	 * Configure PMP at last because if SMEPMP is detected,
 	 * M-mode access to the S/U space will be rescinded.
