acf06d3a09ca1ebf14a014fb78ce0c24759eb060
Qingyu Shang <2931013282@sjtu.edu.cn>
2023-11-02 14:32:16 +0800

    fw_dynamic: add build and run script for test
    Signed-off-by: Qingyu Shang <2931013282@sjtu.edu.cn>

--- test_package/run_payload.sh
+++ test_package/run_payload.sh
@@ -6,8 +6,7 @@ while ! nc -z 127.0.0.1 54320 || ! nc -z 127.0.0.1 54321; do sleep 1; done
   -dtb ./qemu-virt-new.dtb  \
   -m 4G,slots=2,maxmem=8G -object memory-backend-ram,size=2G,id=m0 -object memory-backend-ram,size=2G,id=m1   \
   -numa node,nodeid=0,memdev=m0 -numa node,nodeid=1,memdev=m1 -smp 2,sockets=2,maxcpus=2     \
-  -bios ./PenglaiZone/build/platform/generic/firmware/fw_dynamic.elf    \
-  -device loader,file=STANDALONE_MM.fd,addr=0x80C00000 \
+  -bios ./PenglaiZone/build/platform/generic/firmware/fw_payload.elf    \
   -blockdev node-name=pflash0,driver=file,read-only=on,filename=./RISCV_VIRT_CODE.fd     \
   -blockdev node-name=pflash1,driver=file,filename=./RISCV_VIRT_VARS.fd     \
   -serial tcp:localhost:54320 -serial tcp:localhost:54321     \
