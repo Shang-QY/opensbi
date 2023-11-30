1d79fa9c143b6a5162c3ad88553b1e0a646bc561
Yong Li <yong.li@intel.com>
2023-11-30 15:05:10 +0800

    lib: sbi_ecall: Add a test ecall interface to show the context switch… … case in OpenSBI.
    Signed-off-by: Qingyu Shang <2931013282@sjtu.edu.cn>

---
+++ lib/sbi/sbi_ecall_test.c
@@ -0,0 +1,87 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 Intel Corporation.
+ *
+ * Authors:
+ *   Yong Li <yong.li@intel.com>
+ */
+
+#include <sbi/sbi_ecall.h>
+#include <sbi/sbi_ecall_interface.h>
+#include <sbi/sbi_error.h>
+#include <sbi/sbi_trap.h>
+#include <sbi/sbi_domain.h>
+#include <sbi/sbi_console.h>
+#include <sbi/sbi_string.h>
+
+#define SECURE_DOMAIN   "trusted-domain"
+
+static int sbi_ecall_test_secure_enter(void)
+{
+	int i;
+	struct sbi_domain *dom, *tdom = NULL;
+	sbi_domain_for_each(i, dom)
+	{
+		if (!sbi_strcmp(dom->name, SECURE_DOMAIN)) {
+			tdom = dom;
+			break;
+		}
+	}
+	if (tdom)
+		sbi_domain_switch_context(tdom);
+	return 0;
+}
+
+static int sbi_ecall_test_secure_exit(void)
+{
+	int i;
+	struct sbi_domain *dom, *tdom = NULL;
+	sbi_domain_for_each(i, dom)
+	{
+		if (sbi_strcmp(dom->name, SECURE_DOMAIN)) {
+			tdom = dom;
+			break;
+		}
+	}
+	if (tdom)
+		sbi_domain_switch_context(tdom);
+	return 0;
+}
+
+static int sbi_ecall_test_handler(unsigned long extid, unsigned long funcid,
+				  const struct sbi_trap_regs *regs,
+				  unsigned long *out_val,
+				  struct sbi_trap_info *out_trap)
+{
+	int ret = 0;
+
+	switch (funcid) {
+	case SBI_EXT_TEST_SECURE_ENTER:
+		sbi_printf("sbi_ecall_test_handler: SBI_EXT_TEST_SECURE_ENTER\n");
+		ret = sbi_ecall_test_secure_enter();
+		break;
+	case SBI_EXT_TEST_SECURE_EXIT:
+		sbi_printf("sbi_ecall_test_handler: SBI_EXT_TEST_SECURE_EXIT\n");
+		ret = sbi_ecall_test_secure_exit();
+		break;
+	default:
+		ret = SBI_ENOTSUPP;
+	}
+
+	return ret;
+}
+
+struct sbi_ecall_extension ecall_test;
+
+static int sbi_ecall_test_register_extensions(void)
+{
+	return sbi_ecall_register_extension(&ecall_test);
+}
+
+struct sbi_ecall_extension ecall_test = {
+	.extid_start		= SBI_EXT_TEST,
+	.extid_end		= SBI_EXT_TEST,
+	.register_extensions	= sbi_ecall_test_register_extensions,
+	.handle			= sbi_ecall_test_handler,
+};
